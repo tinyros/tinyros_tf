ded0d3745c95113f8e81dfad7645574ed8aaeda3
Pinkie Fu <363960870@qq.com>
2021-03-06 01:01:12 +0800

    优化包路径

--- roslib/cpp/package.cpp
+++ roslib/cpp/package.cpp
@@ -8,21 +8,24 @@
 namespace tinyros {
 namespace package {
   std::string getPath(std::string package) {
+#ifdef __linux__
     char  exec_path[PATH_MAX+1];
     int  cnt = readlink( "/proc/self/exe" , exec_path, PATH_MAX+1);
     if(cnt > 0 && cnt < (PATH_MAX+1)) {
       for(int i = cnt; i >=0; --i) {
        if(exec_path[i] ==  '/' ) {
           exec_path[i+1] =  '\0' ;
-          std::string packate_path = std::string(exec_path) + "share/" + package + "/";
+          std::string packate_path = std::string(exec_path) + "pkg_" + package + "/";
           return packate_path;
        }
       }
     }
+#endif
     return "";
   }
 
   std::string getExecPath() {
+#ifdef __linux__
     char  exec_path[PATH_MAX+1];
     int  cnt = readlink( "/proc/self/exe" , exec_path, PATH_MAX+1);
     if(cnt > 0 && cnt < (PATH_MAX+1)) {
@@ -33,6 +36,7 @@ namespace package {
        }
       }
     }
+#endif
     return "";
   }
 }
